bdcae343fe43c63536fd0823980c7986c9809201
Alex <A_S293@Student.usc.edu.au>
2018-08-23 18:22:50 +1000

    Last weeks project

--- app/src/main/java/com/bignerdranch/andriod/criminaiintent2/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/andriod/criminaiintent2/SingleFragmentActivity.java
@@ -1,21 +1,25 @@
 package com.bignerdranch.andriod.criminaiintent2;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+/**
+ * Created by A_S293 on 16/08/2018.
+ */
 
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
-
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
         if (fragment == null) {
-            fragment = new CrimeFragment();
+            fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragment_container, fragment)
                     .commit();
