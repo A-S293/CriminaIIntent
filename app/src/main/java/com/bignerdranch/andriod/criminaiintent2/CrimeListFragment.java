719d8b1a3d7423bd582b0ed73174b8e7d326cb66
Alex <A_S293@Student.usc.edu.au>
2018-08-23 18:33:19 +1000

    Listing 8.11 in porgress

---
+++ app/src/main/java/com/bignerdranch/andriod/criminaiintent2/CrimeListFragment.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.andriod.criminaiintent2;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by A_S293 on 23/08/2018.
+ */
+
+public class CrimeListFragment extends Fragment {
+
+// Nothing yet
+
+}
